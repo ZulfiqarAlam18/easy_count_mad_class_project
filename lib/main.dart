1f0f23b3787533c6117f6aff36a46a4cad806009
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2024-09-25 20:15:03 +0500

    worked on theme data in MaterialApp throughly

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,120 @@
 import 'package:easy_count/first_screen.dart';
 import 'package:easy_count/new_match.dart';
+import 'package:easy_count/scorecard.dart';
 import 'package:easy_count/splashScreen.dart';
 import 'package:flutter/material.dart';
 import 'homeScreen.dart';
+import 'demo.dart';
+
 void main(){
   runApp(const EasyCount());
 }
+
 class EasyCount extends StatelessWidget {
   const EasyCount({super.key});
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,// to remove debug banner(icon)
       title: 'EasyCount',
+
       theme: ThemeData(
+
+        primaryColor: Colors.teal, // don't know reason of using this line but appbar and major elements
+        brightness: Brightness.light, //two options either dark or light(white)
+        scaffoldBackgroundColor: Colors.teal[50], //scaffold color
+        fontFamily: 'Roboto', //defualt font family
+
+          //App Bar structure
         appBarTheme: AppBarTheme(
-          color: Colors.teal,
+          backgroundColor: Colors.teal,
+          foregroundColor: Colors.white, //for icons and text
+          elevation: 0, //no idea of using this line
           centerTitle: true,
-          titleTextStyle: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),
-          iconTheme: IconThemeData(
-            color: Colors.white,
-          )
-        )
+
+        ),
+
+          // Elevated Buttons
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.teal,
+            foregroundColor: Colors.white,
+            padding: EdgeInsets.symmetric(horizontal: 24,vertical: 12),
+            textStyle: TextStyle(
+            //  fontWeight: FontWeight.w500,
+            //  fontSize: 18,
+
+            )
+          ),
       ),
-     // initialRoute: '/',
+
+        //for textField
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: EdgeInsets.symmetric(horizontal: 24,vertical: 12),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: Colors.teal,
+              width: 2.0,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.teal,
+              width: 2.0,
+            ),
+                borderRadius: BorderRadius.circular(12),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12.0),
+            borderSide: BorderSide(
+              color: Colors.teal,
+              width: 2.0,
+            )
+          ),
+          labelStyle: TextStyle(
+            color: Colors.teal,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          )
+
+        ),
+        //text themes
+          textTheme: TextTheme(
+            headlineLarge: TextStyle(fontWeight: FontWeight.bold,fontSize: 24,color: Colors.teal),
+            headlineMedium: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.teal),
+            headlineSmall: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.teal),
+
+          ),
+
+
+
+        //for icons
+        iconTheme: IconThemeData(
+          color: Colors.teal,
+          size: 24,
+
+        )
+
+
+
+
+      ),
+
+      // initialRoute: '/',
+
       routes: {
       //  '/': (context) => SplashScreen(),
         '/f_screen': (context) => FirstScreen(),
         '/home': (context) => HomeScreen(),
         '/newMatch': (context) => NewMatch(),
       },
-      home: SplashScreen(),
+
+      home: ScoreboardScreen(),
+
     );
+
   }
 }
