8b23dd0005a6d45b304d50d8f3bb9161d23b22e1
ZulfiqarAlam18 <zulfi18.official@gmail.com>
2024-09-27 20:23:45 +0500

    ui of summaryScreen ready

---
+++ lib/summaryScreen.dart
@@ -0,0 +1,44 @@
+import 'package:easy_count/demo.dart';
+import 'package:easy_count/firstInnings.dart';
+import 'package:flutter/material.dart';
+import 'secondInnings.dart';
+
+class Summaryscreen extends StatefulWidget{
+  const Summaryscreen({super.key});
+
+  @override
+  AppState createState() => AppState();
+
+}
+
+class AppState extends State <Summaryscreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Match Summary '),
+          bottom: TabBar(
+            tabs: [
+              Tab(child: Text('1st Innings',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),),
+              Tab(child: Text('2nd Innings',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),),
+
+            ],
+            labelColor: Colors.white,
+          ),
+        ),
+      body: TabBarView(
+        children: [
+          FirstInnings(),
+          SecondInnings(),
+        ]
+
+
+      ),
+      ),
+    );
+  }
+
+}
